d00fa4ce0a3cc8f7da3bbde71540868448d56eb9
NSFW <nsfw@outlook.com>
2026-02-27 22:02:18 -0800

    Statiq initialized

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+public class Program
+{
+  public static async Task<int> Main(string[] args) =>
+    await Bootstrapper
+      .Factory
+      .CreateWeb(args)
+      .RunAsync();
+}
